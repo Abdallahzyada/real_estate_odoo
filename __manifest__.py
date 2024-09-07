496203d561cc6055bbb58962cdb4dafd5cc66459
abdallah zyada <abdallahzyada25@gmail.com>
2024-09-07 23:48:00 +0300

    adding tree view

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Real Estate',
+    'technical_name': 'real_estate',
+    'description': 'a simple app',
+    'author': 'abdallah zyada',
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base', 'contacts', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/property_view.xml',
+    ],
+    'application': True
+}
