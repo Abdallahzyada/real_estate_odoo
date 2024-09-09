6a6d11181cd93d0e439adcdac92ac4e639a8a926
abdallah zyada <abdallahzyada25@gmail.com>
2024-09-09 22:23:31 +0300

    applying validation

--- models/property.py
+++ models/property.py
@@ -1,11 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class Property(models.Model):
     _name = 'property'
 
-    name = fields.Char()
+    name = fields.Char(required=1, )
     description = fields.Text()
-    post_code = fields.Char()
+    post_code = fields.Char(required=1, size=6)
     date_availability = fields.Date()
     expected_price = fields.Float()
     selling_price = fields.Float()
@@ -19,5 +21,26 @@ class Property(models.Model):
         [('east', 'East'),
         ('north', 'Nort'),
         ('south', 'South'),
-        ('west', 'West')]
+        ('west', 'West')],
+        default='east'
     )
+
+    _sql_constraints = [('unique_name','unique("name")','This Name Already exists!')]
+    @api.constrains('bedrooms')
+    def check_bedrooms_greater_zero(self):
+        for rec in self:
+            if rec.bedrooms <= 0:
+                raise ValidationError('Please add a valid number of bedrooms!')
+
+    #create
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super(Property, self).create(vals_list)
+        #logic
+        return res
+    #write
+    @api.model
+    def _search(self, domain, offset=0, limit=None, order=None, access_rights_uid=None):
+        res = super(Property, self).search(domain, offset=0, limit=None, order=None, access_rights_uid=None)
+        #logic
+        return res
