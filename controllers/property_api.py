986eb1902f00e823b91f22515f5c7ae0aa99bb41
abdallah zyada <abdallahzyada25@gmail.com>
2024-10-03 14:43:05 +0300

    Get list of records with filtration

--- controllers/property_api.py
+++ controllers/property_api.py
@@ -1,6 +1,7 @@
 import json
 from odoo import http
 from odoo.http import request
+from python.Lib.urllib.parse import parse_qs
 
 
 class PropertyApi(http.Controller):
@@ -71,8 +72,57 @@ class PropertyApi(http.Controller):
                 "post_code": property_id.post_code,
                 "bedrooms": property_id.bedrooms,
             }, status=200)
+
         except Exception as error:
             return request.make_json_response({
                 'message': error,
             }, status=400)
 
+
+    @http.route("/v1/property/<int:property_id>", methods=["DELETE"], type='http', auth='none', csrf=False)
+    def delete_property(self, property_id):
+        try:
+            property_id = request.env['property'].sudo().search([('id', '=', property_id)])
+            if not property_id:
+                return request.make_json_response({
+                    'message': 'Record not found!'
+                }, status=400)
+
+            args = request.httprequest.data.decode()
+            vals = json.loads(args)
+            property_id.unlink()
+            return request.make_json_response({
+                "message": "record deleted"
+            }, status=200)
+
+        except Exception as error:
+            return request.make_json_response({
+                'message': error,
+            }, status=400)
+
+
+    @http.route("/v1/properties", methods=["GET"], type='http', auth='none', csrf=False)
+    def get_property_list(self):
+        try:
+            params = parse_qs(request.httprequest.query_string.decode('utf-8'))
+            property_domain = []
+            if params.get('state'):
+                property_domain += [('state', '=', params.get('state')[0])]
+            property_ids = request.env['property'].sudo().search(property_domain)
+            if not property_ids:
+                return request.make_json_response({
+                    "message": "No record found",
+                }, status=400)
+
+            return request.make_json_response([{
+                "id": property.id,
+                "ref": property.ref,
+                "name": property.name,
+                "post_code": property.post_code,
+                "bedrooms": property.bedrooms,
+            } for property in property_ids], status=200)
+
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
